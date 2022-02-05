d903ff73f23d661e1e48fdf35b68df7dfeaa7680
AbelSL <abelsl1999@gmail.com>
2022-02-04 18:27:40 -0600

    Se vió console.log, operadores matemáticos, formato y concatenación de texto

---
+++ Chapter 1/arth-&-str.js
@@ -0,0 +1,23 @@
+// Imprimir mensajes
+console.log('IMPRESIÓN DE TEXTO')
+console.log("Hola mundo \nFraternales abrazos")
+console.log("***********************")
+console.log('NOTA: Un salto de linea se hace con \\n\.')
+console.log("\n")
+
+// Operadores
+console.log("OPERADORES MATEMÁTICOS")
+console.log('La suma de 5 y 3 es: ',5+3)
+console.log("La resta de 5 y 3 es: ",5-3)
+console.log('La multiplicación de 5 y 3 es: ',5*3)
+console.log("La division de 5 y 3 es: ",5/3)
+//console.log("EL resto de 5 y 3 es: ",5%3)
+console.log('\n')
+
+//Concatenación
+console.log("Con"+"ca"+"tena"+"ción")
+console.log('\n')
+
+//Formato de Texto
+console.log("Formato de texto")
+console.log(`mitad de 100 es ${100/2}`)
