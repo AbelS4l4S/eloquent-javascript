3fbede6c94e098adc23b0cbb07156d9958cf0278
AbelSL <abelsl1999@gmail.com>
2022-02-04 19:52:32 -0600

    Se añadieron operadores lógicos

--- Chapter 1/comparison.js
+++ Chapter 1/comparison.js
@@ -1,3 +1,4 @@
+//Comparadores
 console.log("¿3 es mayor que 2?",3>2)
 console.log("¿2 es mayor que 3?",3<2)
 console.log("¿6 es igual a 4?",6==4)
@@ -7,3 +8,15 @@ console.log("¿6 es mayor o igual a 8?",6>=8)
 
 //Exepción, NaN nunca será igual a NaN
 console.log(NaN==NaN)
+
+//Operador lógicos AND
+console.log(false&&false)
+console.log(false&&true)
+console.log(true&&false)
+console.log(true&&true)
+
+//Operador lógicos OR
+console.log(false||false)
+console.log(false||true)
+console.log(true||false)
+console.log(true||true)
