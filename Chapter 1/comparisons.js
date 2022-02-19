7c3d00f1ffc07e2fbf5c13b0826937aa1a3c501f
AbelSL <abelsl1999@gmail.com>
2022-02-19 15:38:57 -0600

    conclusión del capitulo uno

---
+++ Chapter 1/comparisons.js
@@ -0,0 +1,13 @@
+//Comparadores
+console.log("Comparadores ")
+console.log("¿3 es mayor que 2?",3>2)
+console.log("¿2 es mayor que 3?",3<2)
+console.log("¿6 es igual a 4?",6==4)
+console.log("¿6 no es igual a 8?",6!=8)
+console.log("¿6 es menor o igual a 8?",6<=8)
+console.log("¿6 es mayor o igual a 8?",6>=8)
+console.log("\n")
+
+//Exepción, NaN nunca será igual a NaN
+console.log("¿NaN == NaN?",NaN==NaN)
+console.log("\n")
--- Chapter 1/logical-operators.js
+++ Chapter 1/logical-operators.js
@@ -1,13 +1,3 @@
-//Comparadores
-console.log("Comparadores ")
-console.log("¿3 es mayor que 2?",3>2)
-console.log("¿2 es mayor que 3?",3<2)
-console.log("¿6 es igual a 4?",6==4)
-console.log("¿6 no es igual a 8?",6!=8)
-console.log("¿6 es menor o igual a 8?",6<=8)
-console.log("¿6 es mayor o igual a 8?",6>=8)
-console.log("\n")
-
 //Exepción, NaN nunca será igual a NaN
 console.log("¿NaN == NaN?",NaN==NaN)
 console.log("\n")
@@ -27,3 +17,7 @@ console.log("false or true ->",false||true)
 console.log("true or false ->",true||false)
 console.log("true or true ->",true||true)
 console.log("\n")
+
+//Operador lógico ternario
+console.log(true ? 1:2);
+console.log(false ? 1:2);
