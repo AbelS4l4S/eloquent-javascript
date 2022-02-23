dda0e98db23211aa0247a462c383cd8703c204e9
AbelSL <abelsl1999@gmail.com>
2022-02-22 22:14:25 -0600

    Se añadio el tema de switch

---
+++ Chapter 2/Dispatching on a value with switch.js
@@ -0,0 +1,16 @@
+let clima="Soleado";
+
+switch(clima){
+case "Soleado":
+    console.log("Recuerda llevar un parasol");
+    break;
+
+    case "lluviado":
+    console.log("No te vallas a mojar mucho");
+    break;
+
+    case "Nublado":
+    console.log("Sal a caminar");
+    break;
+
+}
